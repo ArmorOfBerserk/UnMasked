b70a2f3bef5bed4cbef795f4e921c6c62f393083
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-01-31 19:10:09 +0100

    feat: add menu manager

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("start game");
+    }
+    public void GoToOptions()
+    {
+        Debug.Log("options");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit game");
+    }
+}
