0451cc54f824e4a13d8d4213ab0946bcb5fe68b3
Simone Bilotta <67840842+ArmorOfBerserk@users.noreply.github.com>
2026-01-31 23:10:59 +0100

    Aggiunta death box + particellare salto

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,18 +6,11 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput playerInput;
     private Animator anim;
     private SpriteRenderer spriteRenderer;
-    
-    // Riferimento all'oggetto interagibile
     private IInteractable currentInteractable;
 
     [Header("Variabili di movimento")]
     [SerializeField] float movSpeed;
     [SerializeField] float jumpForce;
-    private InputAction moveAction;
-    private InputAction jumpAction;
-
-
-
     [SerializeField] private Rigidbody2D rb;
 
     [Header("GroundCheck")]
@@ -29,91 +18,84 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private LayerMask groundLayer;
 
-    private bool isOnGround => groundCheck != null && Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    
-    private Vector2 moveInput;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction interactAction;
+    //Da settare a mano per ogni livello
+    public Transform startPoint;
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        if(groundCheck != null)
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-    }
+    private Vector2 moveInput;
+    private bool wasInAir;
+    private bool isOnGround => groundCheck != null && Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+    [Header("Effetti Atterraggio")]
+    [SerializeField] private GameObject dustPrefab;
+    [SerializeField] private float fastFallThreshold = -10f;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-        
+
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
-
+        interactAction = playerInput.actions["Interact"];
     }
 
     void OnEnable()
     {
         moveAction.performed += OnMove;
-        moveAction.canceled  += OnMoveCanceled;
-        
+        moveAction.canceled += OnMoveCanceled;
         jumpAction.performed += OnJump;
+        if (interactAction != null) interactAction.performed += OnInteractTriggered;
     }
 
     void OnDisable()
     {
         moveAction.performed -= OnMove;
-        moveAction.canceled  -= OnMoveCanceled;
-        
+        moveAction.canceled -= OnMoveCanceled;
         jumpAction.performed -= OnJump;
+        if (interactAction != null) interactAction.performed -= OnInteractTriggered;
     }
-    
-    
+
+    private void OnMove(InputAction.CallbackContext ctx) => moveInput = ctx.ReadValue<Vector2>();
+    private void OnMoveCanceled(InputAction.CallbackContext ctx) => moveInput = Vector2.zero;
+
     private void OnJump(InputAction.CallbackContext ctx)
     {
-        if (!isOnGround) return;
-
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-
-        anim.SetBool("isJumping", true);
-    }
-
-        // INTERAZIONE IMMEDIATA
-        // "performed" scatta al frame esatto della pressione
-        var interactAction = playerInput.actions.FindAction("Interact");
-        if (interactAction != null)
+        if (isOnGround)
         {
-            interactAction.performed += ctx => TryInteract();
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            anim.SetBool("isJumping", true);
         }
     }
 
-    
-    private void OnMove(InputAction.CallbackContext ctx)
+    private void OnInteractTriggered(InputAction.CallbackContext ctx)
     {
-        moveInput = ctx.ReadValue<Vector2>();
+        TryInteract();
     }
 
-    private void OnMoveCanceled(InputAction.CallbackContext ctx)
+    private void TryInteract()
     {
-        moveInput = Vector2.zero;
-    }
-
-
-
-    /*private void JumpAction()
-    {
-        // Se c'è un oggetto nel raggio, interagisce subito
         if (currentInteractable != null)
         {
             currentInteractable.Interact();
         }
     }
 
-    // Rilevamento collisioni per interazione
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out IInteractable interactable))
-        {
             currentInteractable = interactable;
+
+        if (collision.tag == "DeathBox")
+        {
+            /*  manager.Hurt(); */
+            Debug.Log("CIAO");
+            transform.position = startPoint.position;
+            transform.rotation = startPoint.rotation;
+
         }
     }
 
@@ -121,48 +103,42 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.TryGetComponent(out IInteractable interactable))
         {
-            if (currentInteractable == interactable)
-            {
-                currentInteractable = null;
-                // Chiude la finestra se ti allontani
-                if (InteractionUI.Instance != null && InteractionUI.Instance.IsOpen)
-                    InteractionUI.Instance.CloseWindow();
-            }
+            if (currentInteractable == interactable) currentInteractable = null;
         }
-    }
-
-    //}
-
-    private void FlipCharacter()
-    {
-        if (moveInput.x > 0.1f)
-            spriteRenderer.flipX = false;
-        else if (moveInput.x < -0.1f)
-            spriteRenderer.flipX = true;
-    }
-
-    private void AnimateCharacter()
-    {
-        // Logica animazione qui
-    }
-
-    private void MoveAction()
-    {
-        rb.linearVelocity = new Vector2(moveInput.x * movSpeed, rb.linearVelocity.y);
-        anim.SetBool("move", true);
     }
 
     void Update()
     {
         FlipCharacter();
-        AnimateCharacter();
     }
 
     void FixedUpdate()
-    {    
+    {
         rb.linearVelocity = new Vector2(moveInput.x * movSpeed, rb.linearVelocity.y);
 
-        anim.SetBool("move", Mathf.Abs(moveInput.x) > 0.01f);
+        // Logica polvere/atterraggio
+        if (!isOnGround) wasInAir = true;
+        else if (wasInAir)
+        {
+            if (rb.linearVelocity.y < fastFallThreshold) SpawnDust();
+            wasInAir = false;
+            anim.SetBool("isJumping", false);
+        }
+
 
     }
+
+    private void FlipCharacter()
+    {
+        if (Mathf.Abs(moveInput.x) > 0.1f)
+            spriteRenderer.flipX = moveInput.x < 0;
+    }
+
+    private void SpawnDust()
+    {
+        if (dustPrefab != null && groundCheck != null)
+            Instantiate(dustPrefab, groundCheck.position, Quaternion.identity);
+    }
+
+
 }
